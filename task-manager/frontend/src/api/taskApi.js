d32520d75e5a94d015da016728f365bb643b8ab1
ayushi-309 <tayushi751@gmail.com>
2026-06-18 12:38:52 +0530

    Update frontend API URL

--- task-manager/frontend/src/api/taskApi.js
+++ task-manager/frontend/src/api/taskApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:5000/api/tasks";
+const API_URL = "https://ayushi-taskmanager-backend.onrender.com/api/tasks";
 
 export const createTask = async (taskData) => {
   const response = await axios.post(API_URL, taskData);
